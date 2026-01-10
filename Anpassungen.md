acbe07e11a2b16e75179841ebf73b54ab2984e28
Bernhard <bernhard.gailer@gmx.de>
2026-01-10 18:04:18 +0100

    add files

---
+++ Anpassungen.md
@@ -0,0 +1,3 @@
+LaTeX Workshop Settings:
+* `"latex-workshop.latex.autoBuild.run": "onSave"`
+* `"latex-workshop.latex.outDir": "build"`
---
+++ Grundlagen/beispiel.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Ausgabe auf der Konsole
+        System.out.println("Hallo Welt!");
+        // Kleine Beispielberechnung
+        int a = 5;
+        int b = 3;
+        int summe = a + b;
+        System.out.println("Die Summe von " + a + " und " + b + " ist: " + summe);
+    }
+}
+
